c9ec098b79f600ac533754a2f7168077f72441c7
Shivermist <shivermist0104@gmail.com>
2024-03-11 23:30:20 -0400

    added functional main menu

---
+++ CanvasLayer.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@onready var menu = $Menu
+
+
+
+
+
+func _on_button_play_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_button_quit_pressed():
+	get_tree().quit()

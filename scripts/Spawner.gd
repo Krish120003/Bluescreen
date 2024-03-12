ad2aeff439b173e091f0738ded8694c2450f1cf4
Rachelle DeMan <demanr@mcmaster.ca>
2024-03-12 02:16:25 -0400

    Added variety in size

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -3,10 +3,11 @@ extends Marker2D
 var popup = preload("res://scenes/popup.tscn")
 @onready var timer = get_node("Timer")
 
-@export var handicap = 3
+@export var handicap = 0
 @export var storage = 0
 
-var differenceInSpawn = 4
+var differenceInSpawn = 5
+var borderBuffer = 100
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,9 +26,14 @@ func resetSpawn():
 func _on_timer_timeout():
 	var popupSpawned = popup.instantiate()
 	# get_parent
-	#var e = get_parent().texture.get_size()
-	var xDif = randi() % 550
-	var yDif = randi() % 250
+	var windowSize = get_parent().get_parent().get_size() 
+	var xDif = randi() % (int(windowSize[0]/2)) - (int(windowSize[0]/8)) # add buffer limit so not out of bounds
+	var yDif = randi() % (int(windowSize[1]/2)) - (int(windowSize[1]/8)) # add buffer limit so not out of bounds
+	# add additional randomness for negative
+	if randi() % 2 == 0:
+		xDif = -xDif
+	if randi() % 2 == 0:
+		yDif = -yDif
 	popupSpawned.position = Vector2(xDif,yDif)
 	add_child(popupSpawned)
 	resetSpawn()
--- scripts/popupWindow.gd
+++ scripts/popupWindow.gd
@@ -8,6 +8,12 @@ var warningSpawnTime = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# set custom size
+	#var xStretch = randi() % 9
+	#var yStretch = randi() % 5
+	#scale.x += xStretch / 3
+	#scale.y += yStretch / 3
+	
 	timer.set_wait_time(originalSpawnTime)
 	timer.start()
 
